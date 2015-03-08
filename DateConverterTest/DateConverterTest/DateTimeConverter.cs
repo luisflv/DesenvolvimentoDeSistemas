6cd987f291dc3639c41dd80bd5088d79f73e2081
luisflv <luis_flv@hotmail.com.br>
2015-03-08 20:40:28 -0300

    Teste - Um minutoi atras

---
+++ DateConverterTest/DateConverterTest/DateTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateConverterTest
+{
+    public class DateTimeConverter
+    {
+
+        public string ToDate(DateTime now, DateTime post)
+        {
+            var dif = post - now;
+
+            if (dif.Minutes == 1)
+            {
+                return dif.Minutes + (" minuto atras");
+            }
+
+            else
+            {
+                return dif.Minutes + (" minutos atras");
+            }
+         
+        }
+
+    }
+}
