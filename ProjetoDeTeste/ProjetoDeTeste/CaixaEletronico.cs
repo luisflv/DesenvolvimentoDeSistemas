c1c1105d16959915c99a3c14210405140bff3cdd
luisflv <luisflv@gmail.com>
2015-03-16 20:46:34 -0300

    Enviando exercicio Caixa Eletronico

---
+++ ProjetoDeTeste/ProjetoDeTeste/CaixaEletronico.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDeTeste
+{
+    public class CaixaEletronico
+    {
+        public double Saldo
+        {
+            get
+            {
+               return sal;
+            }
+            set
+            {
+                sal = value; 
+            }
+        }
+
+        private double sal = 0;
+
+        public void Depositar(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("VALOR INVÁLIDO", "valor");
+            }
+            else
+            {
+                Saldo += valor;
+            }          
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= 0)
+            {
+                Saldo = 0;
+            }
+            
+            Saldo = Saldo - valor;
+        }
+
+
+    }
+}
---
+++ ProjetoDeTeste/ProjetoDeTeste/CaixaEletronicoTeste.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjetoDeTeste
+{
+    [TestClass]
+    public class CaixaEletronicoTeste
+    {
+        private CaixaEletronico _caixaEletronico;
+
+        [TestInitialize]
+        public void InicializarTeste()
+        {
+            _caixaEletronico = new CaixaEletronico();
+        }
+
+
+        [TestMethod]
+        public void Saldo_Do_Caixa_Eletronico_Deve_Iniciar_Com_Zero()
+        {
+
+            Assert.AreEqual(0,_caixaEletronico.Saldo);
+        }
+        
+       [TestMethod]
+        public void Quando_Depositar_20_Retornar_Saldo_20()
+        {
+            _caixaEletronico.Depositar(20);
+            Assert.AreEqual(20, _caixaEletronico.Saldo);
+
+        }
+
+
+        [TestMethod]
+        public void Quando_Depositar_10_E_Depois_Depositar_30_Retornar_40()
+        {
+
+            _caixaEletronico.Depositar(10);
+            _caixaEletronico.Depositar(30);
+
+            Assert.AreEqual(40, _caixaEletronico.Saldo);
+
+        }
+
+        [TestMethod]
+        public void Quando_Sacar_10_E_No_Caixa_Tiver_25_Retornar_15()
+        {
+            _caixaEletronico.Depositar(25);
+            _caixaEletronico.Sacar(10);
+
+            Assert.AreEqual(15,_caixaEletronico.Saldo);
+        }
+      
+        [TestMethod]
+        public void Quando_Sacar_50_E_No_Caixa_Tiver_70_Retornar_20()
+        {
+
+            _caixaEletronico.Depositar(70);
+            _caixaEletronico.Sacar(50);
+
+            Assert.AreEqual(20,_caixaEletronico.Saldo);
+
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+
+
+        public void Quando_Depositar_Valores_Negativos_Lancar_Exception()
+        {
+            _caixaEletronico.Depositar(-30);
+
+            
+        }
+
+
+    }
+}
